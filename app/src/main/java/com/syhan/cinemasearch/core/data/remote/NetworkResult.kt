4d507e1b8bfb8346d789c1b4992d0d367b6afa6e
syhan <oboznyy.maks99@gmail.com>
2025-05-31 18:21:25 +0700

    create NetworkResult.kt

---
+++ app/src/main/java/com/syhan/cinemasearch/core/data/remote/NetworkResult.kt
@@ -0,0 +1,17 @@
+package com.syhan.cinemasearch.core.data.remote
+
+sealed interface NetworkResult<T> {
+    data class Success<T>(val data: T) : NetworkResult<T>
+    data class Error<T>(val type: NetworkError) : NetworkResult<T>
+    data class Exception<T>(val exception: Throwable) : NetworkResult<T>
+}
+
+sealed interface NetworkError {
+    data object NoInternet : NetworkError
+    data object EmptyHttpBody : NetworkError
+    data class UnexpectedHttpResponse(val code: Int) : NetworkError
+}
+
+class EmptyHttpBodyException(
+    msg: String = "Http body is empty"
+) : Exception(msg)
