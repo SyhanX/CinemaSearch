9c0c9389450daa50f576021bcb6b9fe40f0120c2
syhan <oboznyy.maks99@gmail.com>
2025-06-02 13:30:38 +0700

    create ObserveWithFragmentLifecycle.kt

---
+++ app/src/main/java/com/syhan/cinemasearch/core/data/ObserveWithFragmentLifecycle.kt
@@ -0,0 +1,24 @@
+package com.syhan.cinemasearch.core.data
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+inline fun <reified T> Flow<T>.observeWithFragmentLifecycle(
+    fragment: Fragment,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    noinline action: suspend (T) -> Unit
+): Job {
+    return fragment.viewLifecycleOwner.lifecycleScope.launch {
+        flowWithLifecycle(
+            lifecycle = fragment.viewLifecycleOwner.lifecycle,
+            minActiveState = minActiveState
+        ).collect {
+            action(it)
+        }
+    }
+}
