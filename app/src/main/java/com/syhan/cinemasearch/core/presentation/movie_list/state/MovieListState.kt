adea9df1f6d8725041ffd3b46746cba8e59355a7
syhan <oboznyy.maks99@gmail.com>
2025-06-03 21:17:31 +0700

    remove unneeded code

--- app/src/main/java/com/syhan/cinemasearch/core/presentation/movie_list/state/MovieListState.kt
+++ app/src/main/java/com/syhan/cinemasearch/core/presentation/movie_list/state/MovieListState.kt
@@ -1,9 +1,7 @@
 package com.syhan.cinemasearch.core.presentation.movie_list.state
 
-import androidx.compose.runtime.Immutable
 import com.syhan.cinemasearch.core.data.UiState
 
-@Immutable
 data class MovieListState(
     val uiState: UiState = UiState.ShowLoading,
     val movies: List<MovieItemState> = emptyList(),
