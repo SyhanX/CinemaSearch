7ab5b95ca663fb78ca90c5d37646cbfd42bd53f0
syhan <oboznyy.maks99@gmail.com>
2025-06-06 19:53:19 +0700

    refactor code

--- app/src/main/java/com/syhan/cinemasearch/core/di/AppModule.kt
+++ app/src/main/java/com/syhan/cinemasearch/core/di/AppModule.kt
@@ -1,8 +1,8 @@
 package com.syhan.cinemasearch.core.di
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import com.syhan.cinemasearch.core.data.remote.RetrofitConstants
 import com.syhan.cinemasearch.core.data.remote.MovieApi
+import com.syhan.cinemasearch.core.data.remote.RetrofitConstants
 import com.syhan.cinemasearch.core.data.repository.MovieRepositoryImpl
 import com.syhan.cinemasearch.core.domain.repository.MovieRepository
 import com.syhan.cinemasearch.core.presentation.movie_details.MovieDetailsViewModel
@@ -15,6 +15,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
+// Due to the small scale of the project I found it unnecessary to have multiple DI modules
 val appModule = module {
     single {
         val interceptor = HttpLoggingInterceptor()
