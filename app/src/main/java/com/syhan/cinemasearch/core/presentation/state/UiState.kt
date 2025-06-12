6f1229a5ece7a54171fd8492b75da65e0d88fd6a
syhan <oboznyy.maks99@gmail.com>
2025-06-12 15:26:23 +0700

    remove redundant code

--- app/src/main/java/com/syhan/cinemasearch/core/presentation/state/UiState.kt
+++ app/src/main/java/com/syhan/cinemasearch/core/presentation/state/UiState.kt
@@ -1,8 +1,5 @@
 package com.syhan.cinemasearch.core.presentation.state
 
-import androidx.compose.runtime.Immutable
-
-@Immutable
 sealed interface UiState {
     data object Loading : UiState
     data object Error : UiState
